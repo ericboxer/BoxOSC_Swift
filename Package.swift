235de1941c244126a307c10f9b9f22163b7d463a
Eric Boxer <eboxhead@gmail.com>
2021-08-01 10:41:45 -0400

    Removing "_swift" from package name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "BoxOSC_Swift",
+    name: "BoxOSC",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BoxOSC_Swift",
+            name: "BoxOSC",
             targets: ["BoxOSC_Swift"]),
     ],
     dependencies: [
